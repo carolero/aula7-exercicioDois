d5e112d26de793d48fd6ff14b5dccdd8e0504ea1
Carol <carolina.oliveira@zup.com.br>
2019-06-03 15:44:20 -0300

    Realizando a tabuada

--- exercicioDois/src/com/br/exercicio/dois/ExercicioDois.java
+++ exercicioDois/src/com/br/exercicio/dois/ExercicioDois.java
@@ -1,9 +1,25 @@
 package com.br.exercicio.dois;
 
+import java.util.Scanner;
+
 public class ExercicioDois {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Iniciando o scanner
+		Scanner scan = new Scanner(System.in);
+		
+		// Input usuário
+		System.out.println("Informe um número: ");
+		int numeroInformado = scan.nextInt();
+		
+		int multiplicador = 1;
+		
+		for (int contagem = 1; contagem <= 10; contagem++) {
+			int resultado = numeroInformado * multiplicador;
+			System.out.println(numeroInformado + " x " + multiplicador + " = " + resultado);
+			multiplicador++;
+		}
 
 	}
 
